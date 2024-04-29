d132cc3c8fabb33053d65746a0f5c8a17713b52c
sorlros <sorlros2047@gmail.com>
2024-04-29 16:38:44 +0900

    fixed

--- src/app/hooks/use-send-notification-to-backend.ts
+++ src/app/hooks/use-send-notification-to-backend.ts
@@ -14,8 +14,8 @@ const useSendNotificationToBackend = () => {
   const sendNotification = async ({ data, token }: NotificationData) => {
     try {
       // const data = { title, body, time, image };
-      // const basePath = "https://all-you-have-to-do-version2.vercel.app";
-      const basePath = "http://localhost:3000";
+      const basePath = "https://all-you-have-to-do-version2.vercel.app";
+      // const basePath = "http://localhost:3000";
       const res = await fetch(`${basePath}/api/sendNotification`, {
         method: "POST",
         headers: {
