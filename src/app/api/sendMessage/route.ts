54671e485c95055bc75512d5e48807598c945beb
sorlros <sorlros2047@gmail.com>
2024-05-07 15:17:32 +0900

    fixed

--- src/app/api/sendMessage/route.ts
+++ src/app/api/sendMessage/route.ts
@@ -31,9 +31,9 @@ interface MessageParam {
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID as string,
-      clientEmail: process.env.FIREBASE_CLIENT_MAIL as string,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n') as string,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_MAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     })
   });
   console.log("SET SDK");
