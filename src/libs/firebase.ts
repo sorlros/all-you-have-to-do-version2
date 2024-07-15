b0323f2c3dd554c1c2b160a00da08fd3e7749040
sorlros <sorlros2047@gmail.com>
2024-07-15 14:12:20 +0900

    serviceAccountKey 등록

--- src/libs/firebase.ts
+++ src/libs/firebase.ts
@@ -1,10 +1,14 @@
 import admin from 'firebase-admin';
-import serviceAccount from "../../serviceAccountKey.json"
+
+const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n');
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-    // databaseURL: 'https://your-database-name.firebaseio.com'
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      privateKey: privateKey,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    }),
   });
 }
 
